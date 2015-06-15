3969f902cb4148442bbe5e4b9128f0a99deae905
Hugo Peixoto <hugo.peixoto@gmail.com>
2015-06-15 13:20:49 +0100

    Rewrites haskell implementation

--- haskell/mergesort.hs
+++ haskell/mergesort.hs
@@ -1,32 +1,17 @@
-
-list_tail :: [Integer] -> Integer -> [Integer]
-list_tail lst 0 = lst
-list_tail (e:lst) n = list_tail lst (n - 1)
-
-list_head :: [Integer] -> Integer -> [Integer]
-list_head lst 0 = []
-list_head (e:lst) n = e:(list_head lst (n - 1))
-
-merge :: [Integer] -> [Integer] -> [Integer]
 merge a [] = a
 merge [] b = b
-merge (ea:a) (eb:b) =
-	if ea < eb
-	then ea:(merge a (eb:b))
-	else eb:(merge (ea:a) b)
+merge as@(ah:at) bs@(bh:bt) =
+	if ah <= bh
+	then ah:(merge at bs)
+	else bh:(merge as bt)
 
-mergesort :: [Integer] -> [Integer]
-mergesort lst =
-	let ln = toInteger (length lst)
-	in
-		if ln == 1
-		then lst
-		else merge
-			(mergesort (list_head lst (div ln 2)))
-			(mergesort (list_tail lst (div ln 2)))
+mergesort list
+  | half == 0 = list
+  | otherwise =
+      let (a, b) = splitAt half list in
+        merge (mergesort a) (mergesort b)
 
-main =
-	let lst = [10, 9, 8, 4, 5, 6, 7, 3, 2, 1]
-	in
-	do
-		print (mergesort lst)
+  where half = div (length list) 2
+
+main = do
+	print $ mergesort [10, 9, 8, 4, 5, 6, 7, 3, 2, 1]
