6f621dba1831f48ba76d5630728337ab9ad89ddf
Hugo Peixoto <hugo.peixoto@gmail.com>
2018-03-09 23:23:31 +0000

    Cleanup ruby implementation a bit

--- ruby/mergesort.rb
+++ ruby/mergesort.rb
@@ -1,18 +1,15 @@
-
 def merge(a, b)
   r = []
-  r << (a.first < b.first ? a : b).shift while a.length*b.length > 0
-  return r+a+b
+  r << (a.first < b.first ? a : b).shift until a.empty? || b.empty?
+  r + a + b
 end
 
 def mergesort(lst)
   return lst if lst.size == 1
 
-  a = mergesort(lst[0, lst.size/2])
-  b = mergesort(lst[lst.size/2, lst.size])
-  return merge(a, b)
+  a, b = lst.each_slice((lst.size + 1) / 2).to_a
+
+  merge(mergesort(a), mergesort(b))
 end
 
-mlst = [10, 9, 8, 5, 6, 7, 4, 3, 2, 1]
-sorted = mergesort(mlst)
-puts sorted
+puts mergesort([10, 9, 8, 5, 6, 7, 4, 3, 2, 1])
